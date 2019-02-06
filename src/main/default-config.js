fd56fd12d908a236c6811ff74b39c0bf9285cbc6
Austin Kregel <austin@kbcomp.co>
2019-02-05 23:38:19 -0500

    Add and rename default config file

---
+++ src/main/default-config.js
@@ -0,0 +1,33 @@
+/*
+|--------------------------------------------------------------------------
+| Set custom links in your application
+|--------------------------------------------------------------------------
+|
+| These values in this file will be displayed under the "Window" option in
+| the title bar of the application (in the place where you might find actions
+| like "File", "Edit", or "Help"). Then they'll be under "
+|
+*/
+
+module.exports = [
+  {
+    "label": "Netflix",
+    "url": "https://netflix.com/browse"
+  },
+  {
+    "label": "Hulu",
+    "url": "https://hulu.com"
+  },
+  {
+    "label": "YouTube",
+    "url": "https://youtube.com"
+  },
+  {
+    "label": "Twitch",
+    "url": "https://twitch.tv"
+  },
+  {
+    "label": "FloatPlane",
+    "url": "https://floatplane.com"
+  }
+]
