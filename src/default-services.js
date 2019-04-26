3f09978e83b1837eb23d7192894eca9805167d83
Oscar Beaumont <oscartbeaumont@gmail.com>
2019-04-27 00:44:28 +0800

    Scaled Logos to make the UI look correct
    All the logos should be similar sizes even though their dimensions are slightly different.

--- src/default-services.js
+++ src/default-services.js
@@ -1,30 +1,41 @@
-module.exports = [
-  {
-    name: 'Netflix',
-    logo: 'services/netflix.png',
-    url: 'https://netflix.com/browse',
-    color: '#e50914',
-    style: {}
-  },
-  {
-    name: 'YouTube',
-    logo: 'services/youtube.svg',
-    url: 'https://youtube.com',
-    color: '#ff0000',
-    style: {}
-  },
-  {
-    name: 'Twitch',
-    logo: 'services/twitch.svg',
-    url: 'https://twitch.tv',
-    color: '#6441a5',
-    style: {}
-  },
-  {
-    name: 'Floatplane',
-    logo: 'services/floatplane.svg',
-    url: 'https://floatplane.com/',
-    color: '#00aeef',
-    style: {}
-  }
-];
+{
+  "services": [
+    {
+      "name": "Netflix",
+      "logo": "services/netflix.png",
+      "url": "https://netflix.com/browse",
+      "color": "#e50914",
+      "style": {
+        "transform": "scale(1.1)"
+      }
+    },
+    {
+      "name": "YouTube",
+      "logo": "services/youtube.svg",
+      "url": "https://youtube.com",
+      "color": "#ff0000",
+      "style": {
+        "transform": "scale(1.9)"
+      }
+    },
+    {
+      "name": "Twitch",
+      "logo": "services/twitch.svg",
+      "url": "https://twitch.tv",
+      "color": "#6441a5",
+      "style": {
+        "transform": "scale(1.6)"
+      }
+    },
+    {
+      "name": "Floatplane",
+      "logo": "services/floatplane.svg",
+      "url": "https://floatplane.com/",
+      "color": "#00aeef",
+      "style": {
+        "transform": "scale(1.7)"
+      }
+    }
+  ],
+  "alwaysOnTop": true
+}
